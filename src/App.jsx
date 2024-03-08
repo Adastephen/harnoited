907201a12a4a1d364927c3f5e09e67f5a8da2943
Adastephen <adakolestephen09@gmail.com>
2024-03-08 21:55:40 +0100

    header

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from  'react'
+import { useState } from 'react'
+import home from './component/major/homepage/home'
+
+function App() {
+
+  return (
+    <>
+    <home/>
+    </>
+  )
+}
+
+export default App
